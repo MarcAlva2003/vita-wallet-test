b56d58935fe1dbe4fef474eb6632a307533c5227
malvaa <malvarez@aluxion.com>
2025-02-14 16:26:43 -0300

    Add: simple button drop

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,61 @@
-import Button from "@/components/UI/button/button.component";
-import { IconComponent } from "@/components/UI/icon/icon.component";
-import Input from "@/components/UI/input/input.component";
-import { icons } from "@/components/UI/icon/icons";
+'use client'
+
+import Button from '@/components/UI/button/button.component'
+import { ButtonDrop } from '@/components/UI/select/button-drop.component'
+import { IconComponent } from '@/components/UI/icon/icon.component'
+import Input from '@/components/UI/input/input.component'
+import { icons } from '@/components/UI/icon/icons'
 
 export default function Home() {
-  
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <Input placeholder="This is the placeholder" label="This is the label" labelBottom="Help text" iconLeft={<IconComponent icon={icons.bitcoin} />} />
-      <Input error placeholder="This is the placeholder" label="This is the label" labelBottom="Help text" iconLeft={<IconComponent icon={icons.bitcoin} />} />
-      <Input placeholder="This is the placeholder" label="This is the label" labelBottom="Help text" iconRight={<IconComponent icon={icons.bitcoin} />} />
-      <Input error placeholder="This is the placeholder" label="This is the label" labelBottom="Help text" iconRight={<IconComponent icon={icons.bitcoin} />} />
+      <ButtonDrop
+        options={[
+          {
+            label: <IconComponent icon={icons.chile} />,
+            value: 'chile'
+          },
+          {
+            label: <IconComponent icon={icons.bitcoin} />,
+            value: 'bitcoin'
+          },
+          {
+            label: <IconComponent icon={icons.tether} />,
+            value: 'tether'
+          },
+        ]}
+      />
+      <Input
+        placeholder="This is the placeholder"
+        label="This is the label"
+        labelBottom="Help text"
+        iconLeft={<IconComponent icon={icons.bitcoin} />}
+      />
+      <Input
+        error
+        placeholder="This is the placeholder"
+        label="This is the label"
+        labelBottom="Help text"
+        iconLeft={<IconComponent icon={icons.bitcoin} />}
+      />
+      <Input
+        placeholder="This is the placeholder"
+        label="This is the label"
+        labelBottom="Help text"
+        iconRight={<IconComponent icon={icons.bitcoin} />}
+      />
+      <Input
+        error
+        placeholder="This is the placeholder"
+        label="This is the label"
+        labelBottom="Help text"
+        iconRight={<IconComponent icon={icons.bitcoin} />}
+      />
       <Button variant="gradiant">Button Gradiant</Button>
       <Button variant="outlined">Button Outlined</Button>
-      <Button variant="gradiant" disabled>Button Disabled</Button>
+      <Button variant="gradiant" disabled>
+        Button Disabled
+      </Button>
     </div>
-  );
+  )
 }
