b5c354e1f55cebe97a8254cf5ef16669ea312c63
malvaa <malvarez@aluxion.com>
2025-02-14 12:53:32 -0300

    Add: icons and color config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,7 +11,20 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        black: '#010E11',
+        gray: {
+          1: '#B9C1C2',
+          2: '#DEE0E0',
+          3: '#F5F6F6'
+        },
+        white: "#F9F9FA",
+        blue: {
+          1: '#167287',
+          2: '#05BCB9'
+        },
+        red: "#CE3434"
       },
+      'custom-gradient': 'linear-gradient(90deg, #05BCB9 0%, #167287 100%)',
     },
   },
   plugins: [],
