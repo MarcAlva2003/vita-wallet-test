5a2bf173fcf8593156cf35cd76dfc8dc9dc4a903
malvaa <malvarez@aluxion.com>
2025-02-16 18:34:15 -0300

    Add: usa icon

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,2 +1,4 @@
 export {default as MoneyIncomeIcon} from './money-income.svg'
 export {default as DollarBgIcon} from './dollar-bg.svg'
+export {default as SimpleCoinIcon} from './coin.svg'
+export {default as CoinsIcon} from './coins.svg'
