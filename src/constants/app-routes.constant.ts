f0baa8920c49ff25db5dd4df0c503e2e71b21b56
malvaa <malvarez@aluxion.com>
2025-02-16 15:12:17 -0300

    Add: routes

---
+++ src/constants/app-routes.constant.ts
@@ -0,0 +1,10 @@
+export const APP_ROUTES = {
+  HOME: '/dashboard/home',
+  TRANSFER: '/dashboard/home',
+  RECARGE: '/dashboard/home',
+  EXCHANGE: '/dashboard/home',
+  PROFILE: '/dashboard/home',
+  HELP: '/dashboard/home',
+  LOGIN: '/auth/login',
+  REGISTER: '/auth/register',
+}
