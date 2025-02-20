2f7a889352e89ad47b5a53b7fb330a29a55175c1
malvaa <malvarez@aluxion.com>
2025-02-20 12:08:34 -0300

    Fix sidebar current page

--- src/components/sidebar/sidebar.component.tsx
+++ src/components/sidebar/sidebar.component.tsx
@@ -12,8 +12,12 @@ interface ISidebar {
 
 export const Sidebar: React.FC<ISidebar> = (props) => {
   const { navItems } = props
-  const {logout} = useUserToken()
+  const { logout } = useUserToken()
   const pathname = usePathname()
+
+  const isCurrentPath = (path: string) => {
+    return pathname.includes(path)
+  }
 
   return (
     <div className="w-[370px] min-w-[370px] max-w-[370px] h-screen sticky top-0  p-[12vh_92px_64px_0] flex flex-col justify-between relative">
@@ -24,10 +28,10 @@ export const Sidebar: React.FC<ISidebar> = (props) => {
               <Link href={item.path}>
                 <div
                   className={`pl-[80px] w-full py-4 rounded-[0_32px_32px_0] ${
-                    pathname === item.path ? 'bg-blue-2' : ''
+                    isCurrentPath(item.path) ? 'bg-blue-2' : ''
                   }`}
                 >
-                  <h3 className={`text-white ${pathname === item.path ? 'font-semibold' : ''}`}>{item.label}</h3>
+                  <h3 className={`text-white ${isCurrentPath(item.path) ? 'font-semibold' : ''}`}>{item.label}</h3>
                 </div>
               </Link>
             </li>
