7c0ec89ff6cd6666afe1611e011b875c4b0b3531
malvaa <malvarez@aluxion.com>
2025-02-20 12:01:37 -0300

    Add main route redirection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,15 @@ import { APP_ROUTES } from '@/constants/app-routes.constant'
 import { AuthRequired } from '@/HOC/isAuthenticated.hoc'
 import { useEffect } from 'react'
 import { useRouter } from 'next/navigation'
+import { useUserToken } from '@/hooks/useUserToken.hook'
 
 function Home() {
   const { push } = useRouter()
+  const {getAccessToken} = useUserToken()
   useEffect(() => {
+    if (!getAccessToken()){
+      push(APP_ROUTES.LOGIN)
+    }
     push(APP_ROUTES.HOME)
   }, [])
 
