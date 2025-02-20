dabc0130a33ca3c2f12565bbe4c9f23b4127336d
malvaa <malvarez@aluxion.com>
2025-02-20 19:03:42 -0300

    Remove experimental

--- next.config.ts
+++ next.config.ts
@@ -1,24 +1,15 @@
 import type { NextConfig } from 'next'
 import type { WebpackConfigContext } from 'next/dist/server/config-shared'
+
 const nextConfig: NextConfig = {
   webpack: (config: WebpackConfigContext['webpack']) => {
     config.module.rules.push({
       test: /\.svg$/i,
       issuer: /\.[js|ts|jsx|tsx]$/,
-      use: ['@svgr/webpack']
+      use: ['@svgr/webpack'],
     })
     return config
   },
-  experimental: {
-    turbo: {
-      rules: {
-        '*.svg': {
-          loaders: ['@svgr/webpack'],
-          as: '*.js'
-        }
-      }
-    }
-  }
 }
 
 export default nextConfig
